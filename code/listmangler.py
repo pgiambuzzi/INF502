191f7e5bc2a76cee88ce869c91be6c04cb1890a3
pgiambuzzi <52759513+pgiambuzzi@users.noreply.github.com>
2022-09-26 21:16:33 -0700

    Update listmangler.py

--- code/listmangler.py
+++ code/listmangler.py
@@ -2,12 +2,12 @@ def list_mangler(list_in):
     #creating an empty list for our outputs
     list_new = []
     #creating a for loop to run through our original list
-    for item in list_in:
+    for x in list_in:
     #each even number in the given list is multiplied by 2
-        if item % 2 == 0:
-            list_new.append(item*2)
+        if x % 2 == 0:
+            list_new.append(x*2)
     #each odd number in the given list is multiplied by 3
         else:
-            list_new.append(item*3)
+            list_new.append(x*3)
     #and the empty list is populated with our multiplication outputs
     print(list_new)
