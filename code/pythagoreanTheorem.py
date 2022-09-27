9b0d4f78428c90497ddb08682ff2a7717fac15bb
pgiambuzzi <52759513+pgiambuzzi@users.noreply.github.com>
2022-09-27 11:54:33 -0700

    Update pythagoreanTheorem.py

--- code/pythagoreanTheorem.py
+++ code/pythagoreanTheorem.py
@@ -2,6 +2,6 @@
 import math
 
 def pythagoreanTheorem(length_a, length_b):
-   #I felt this was the most straightforward way of making this function, simply printing the result of hypotenuse calculation: c=sqrt(a^2+b^2)
    print(math.sqrt(length_a**2 + length_b**2))
+   #I felt this was the most straightforward way of making this function, simply printing the result of hypotenuse calculation: c=sqrt(a^2+b^2)
   ```
